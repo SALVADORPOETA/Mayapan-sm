2e037981643dd1c02f906abad4bed057052c8371
Salvador Poeta <dsmlhidroponia@gmail.com>
2024-12-10 00:28:52 -0600

    environment variables

--- app/firebase.js
+++ app/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfXPOveU6HYIgCtRIgRlf9kjKYrXR1sBk',
-  authDomain: 'mayapan-sm.firebaseapp.com',
-  projectId: 'mayapan-sm',
-  storageBucket: 'mayapan-sm.firebasestorage.app',
-  messagingSenderId: '763146721844',
-  appId: '1:763146721844:web:8563beb6c912c60d144861',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
