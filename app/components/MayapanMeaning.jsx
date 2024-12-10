1ade9b5ebdfabe046a68ccbce77280c54051647c
Salvador Poeta <dsmlhidroponia@gmail.com>
2024-12-09 22:42:30 -0600

    quot changes

--- app/components/MayapanMeaning.jsx
+++ app/components/MayapanMeaning.jsx
@@ -5,8 +5,8 @@ const MayapanMeaning = () => {
     <div className="mt-6 md:mt-auto lg:mt-6 lg:mb-6 flex justify-center items-center bg-Goldenrod p-5 rounded-md border border-Beige w-full">
       <p className="inline">
         <span className="text-Beige font-bold m-1">Mayapan</span> is the name of
-        one of the biggest cities in the Maya jungle. It means "The flag of the
-        Maya".
+        one of the biggest cities in the Maya jungle. It means &quot;The flag of
+        the Maya&quot;.
       </p>
     </div>
   )
