517eac5c0d21f40c6265236ef5eefe76158c2759
Salvador Poeta <dsmlhidroponia@gmail.com>
2024-12-09 22:54:47 -0600

    legacy

--- app/components/Content.jsx
+++ app/components/Content.jsx
@@ -1,113 +1,113 @@
-// 'use client'
+'use client'
 
-// import Link from 'next/link'
-// import { Link as ScrollLink } from 'react-scroll'
-// import React from 'react'
+import Link from 'next/link'
+import { Link as ScrollLink } from 'react-scroll'
+import React from 'react'
 
-// const Content = () => {
-//   return (
-//     <div className="w-[95%] h-[80%] mt-[90px] bg-Jungle border border-Beige rounded-2xl text-Beige overflow-auto">
-//       <h1 className="mt-6 mx-8 flex justify-center bg-Terracotta rounded-xl p-2 border">
-//         Content
-//       </h1>
-//       <div className="grid grid-cols-2 mx-8 mt-6 mb-4 gap-4">
-//         <div className="overflow-hidden">
-//           <Link href="/" className="text-Goldenrod font-bold px-6">
-//             Home
-//           </Link>
-//           <ul className="mt-2">
-//             <ScrollLink to="1" smooth={true} offset={-100} duration={300}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Introduction
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="2" smooth={true} offset={-100} duration={400}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Geography
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="3" smooth={true} offset={-100} duration={500}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Politics
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="4" smooth={true} offset={-100} duration={600}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Economy
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="5" smooth={true} offset={-100} duration={700}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Religion
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="6" smooth={true} offset={-100} duration={800}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Art
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="7" smooth={true} offset={-100} duration={900}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Architecture
-//               </li>
-//             </ScrollLink>
-//             <ScrollLink to="8" smooth={true} offset={-100} duration={1000}>
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Society
-//               </li>
-//             </ScrollLink>
-//           </ul>
-//         </div>
-//         <div className="overflow-hidden">
-//           <Link href="/history" className="text-Goldenrod font-bold px-6">
-//             History
-//           </Link>
-//           <ul className="mt-2">
-//             <Link href="/history?scrollToId=1">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Pre-Maya Era
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=2">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Early Preclassic
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=3">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Middle Preclassic
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=4">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Late Preclassic
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=5">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Classic Period
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=6">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Postclassic Period
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=7">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Contact Period
-//               </li>
-//             </Link>
-//             <Link href="/history?scrollToId=8">
-//               <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
-//                 Contemporary Maya
-//               </li>
-//             </Link>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
+const Content = () => {
+  return (
+    <div className="w-[95%] h-[80%] mt-[90px] bg-Jungle border border-Beige rounded-2xl text-Beige overflow-auto">
+      <h1 className="mt-6 mx-8 flex justify-center bg-Terracotta rounded-xl p-2 border">
+        Content
+      </h1>
+      <div className="grid grid-cols-2 mx-8 mt-6 mb-4 gap-4">
+        <div className="overflow-hidden">
+          <Link href="/" className="text-Goldenrod font-bold px-6">
+            Home
+          </Link>
+          <ul className="mt-2">
+            <ScrollLink to="1" smooth={true} offset={-100} duration={300}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Introduction
+              </li>
+            </ScrollLink>
+            <ScrollLink to="2" smooth={true} offset={-100} duration={400}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Geography
+              </li>
+            </ScrollLink>
+            <ScrollLink to="3" smooth={true} offset={-100} duration={500}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Politics
+              </li>
+            </ScrollLink>
+            <ScrollLink to="4" smooth={true} offset={-100} duration={600}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Economy
+              </li>
+            </ScrollLink>
+            <ScrollLink to="5" smooth={true} offset={-100} duration={700}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Religion
+              </li>
+            </ScrollLink>
+            <ScrollLink to="6" smooth={true} offset={-100} duration={800}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Art
+              </li>
+            </ScrollLink>
+            <ScrollLink to="7" smooth={true} offset={-100} duration={900}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Architecture
+              </li>
+            </ScrollLink>
+            <ScrollLink to="8" smooth={true} offset={-100} duration={1000}>
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Society
+              </li>
+            </ScrollLink>
+          </ul>
+        </div>
+        <div className="overflow-hidden">
+          <Link href="/history" className="text-Goldenrod font-bold px-6">
+            History
+          </Link>
+          <ul className="mt-2">
+            <Link href="/history?scrollToId=1">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Pre-Maya Era
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=2">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Early Preclassic
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=3">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Middle Preclassic
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=4">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Late Preclassic
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=5">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Classic Period
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=6">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Postclassic Period
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=7">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Contact Period
+              </li>
+            </Link>
+            <Link href="/history?scrollToId=8">
+              <li className="hover:cursor-pointer hover:bg-Goldenrod rounded-lg px-2 hover:py-1 ease-in-out duration-200 border-b">
+                Contemporary Maya
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-// export default Content
+export default Content
