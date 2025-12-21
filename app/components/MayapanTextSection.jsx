f6f2ec75cbae16fbd97f29cd729f219359bec85f
Salvador Poeta <dsmlhidroponia@gmail.com>
2025-12-20 20:19:28 -0600

    change text size 2

--- app/components/MayapanTextSection.jsx
+++ app/components/MayapanTextSection.jsx
@@ -4,7 +4,7 @@ const MayapanTextSection = ({ section }) => {
   return (
     <div className="col-span-12 w-full flex flex-col justify-center items-center md:px-4 md:pb-2">
       <>
-        <p className="md:mt-8 md:mb-4 font-bold text-2xl md:text-4xl">
+        <p className="md:mt-8 md:mb-4 font-bold text-xl md:text-4xl">
           {section}
         </p>
       </>
