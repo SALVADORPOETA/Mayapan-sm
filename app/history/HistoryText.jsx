af1829661a9c7436d34a7f6fd66fb6c002ef00a4
Salvador Poeta <dsmlhidroponia@gmail.com>
2025-12-20 20:33:01 -0600

    change text size 2

--- app/history/HistoryText.jsx
+++ app/history/HistoryText.jsx
@@ -4,7 +4,7 @@ const HistoryText = ({ section, p1, p2 }) => {
   return (
     <div className="justify-center items-center px-4 pb-12 md:mb-0 lg:mb-12 md:py-2">
       <>
-        <p className="mt-8 mb-6 font-bold text-4xl md:hidden lg:flex">
+        <p className="mt-8 mb-6 font-bold text-2xl md:text-4xl md:hidden lg:flex">
           {section}
         </p>
       </>
